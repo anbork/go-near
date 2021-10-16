f3af517314f5e703f3bac3b06c54fe74e4d650be
Evgeny Simonov <workjett@gmail.com>
2021-10-16 17:19:15 +0300

    add hash router

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import {HashRouter as Router, Switch, Route} from 'react-router-dom'
 import {Normalize} from 'styled-normalize'
 
 import {Market, Offer, Product, Profile, Rules} from 'pages'
